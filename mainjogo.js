71f95b93dfb41d359191b2355932711c09d0644a
lesly <leslymontufarj@gmail.com>
2020-07-10 11:03:45 -0300

    teste obstaculos

--- mainjogo.js
+++ mainjogo.js
@@ -1,9 +1,9 @@
-var canvas, ctx, ALTURA, LARGURA, frames=0;
+var canvas, ctx, ALTURA, LARGURA, frames=0, maxPulos=3,
 
 chao = {
-    y: 550,
+    y: 450,
     altura: 50,
-    cor: "#ffdf70",
+    cor: "#aa2323",
 
     desenha: function () {
         ctx.fillStyle = this.cor;
@@ -12,23 +12,66 @@ chao = {
 },
 bloco = {
     x:50,
-    y:0,
+    y:0, //comeca no 0 para fazer o efeito do boquinho caindo
     altura:50,
     largura:50,
     cor:"#ff4e4e",
-    gravidade: 1.5,
+    gravidade: 1.5, //fixo
     velocidade: 0,
-    forcaDoPulo: 15,
+    forcaDoPulo: 15, //na verdade ele pula 15-1.5=13.5 px
     qntPulos: 0,
 
+    atualiza: function() {
+        this.velocidade +=this.gravidade;
+        this.y += this.velocidade; //aqui pula -15 em y
+        if(this.y > chao.y - this.altura) {
+            this.y = chao.y - this.altura; // ele está sempre indo pra baixo...
+            this.qntPulos = 0; //...pela gravidade, ele tbm nao pode ocupar o lugar do chao
+            // dois corpos nao ocupam o mesmo lugar no espaço
+        }
+    },
+    pula: function() {
+        if(this.qntPulos <maxPulos){
+            this.velocidade = -this.forcaDoPulo;
+            this.qntPulos++;
+        }
+    },
     desenha: function () {
         ctx.fillStyle = this.cor;
         ctx.fillRect(this.x, this.y, this.largura, this.altura);
     }
-};
+},
+
+obstaculos = {
+    _obs: [],
+    _cores: ["#ffbc1c", "#ff1c1c", "#ff85e1", "#52a7ff", "#78ff5d"],
+
+    insere: function() {
+        this._obs.push({ //adiciona obs
+            x:200,
+            largura: 30 + Math.floor(20*Math.random),
+            altura: 30 + Math.floor(100*Math.random),
+            cor: this._cores[Math.floor(5*Math.random())]
+        });
+    },
+
+    atualiza: function() {
+
+    },
+
+    desenha: function() {
+        tam = this._obs.length;
+        for (var i = 0; i<tam; i++) { 
+            var obs = this._obs[i]; //cada obs é um objeto
+            ctx.fillStyle = obs.cor;
+            ctx.fillRect(obs.x, chao.y-obs.altura, obs.largura, obs.altura);
+        }
+    }
+}; // até aqui tudo é variavel ou objeto
 
 function clique(event) {
-    // alert("clicou")
+    bloco.pula();
+    obstaculos.insere();
 }
 function main() {
     ALTURA=window.innerHeight;
@@ -60,12 +103,14 @@ function roda() {
 }
 function atualiza() {
     frames++;
+    bloco.atualiza();
 }
 function desenha() {
-    ctx.fillStyle = "#50beff";
+    ctx.fillStyle = "#80daff";
     ctx.fillRect(0,0,LARGURA,ALTURA);
 
     chao.desenha();
+    obstaculos.desenha();
     bloco.desenha();
 }
 
